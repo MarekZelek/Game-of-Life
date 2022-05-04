9f577ab55542ffca288933bacda89e9827b28965
Marek Zelek <mzelek89@gmail.com>
2022-05-04 09:18:49 +0200

    partially working game, nee to handle some issues

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 const numberOfRows = 10;
 const numberOfColumns = 20;
-
+//TODO: take parameters from input
 function generateGameBoard(lines, columns) {
     for (let col = 0; col < columns; col++) {
         const createCol = document.createElement("div");
@@ -19,6 +19,7 @@ function generateGameBoard(lines, columns) {
 let board = new Array(numberOfRows).fill(0).map(() => new Array(numberOfColumns).fill(0));
 let newBoard = new Array(numberOfRows).fill(0).map(() => new Array(numberOfColumns).fill(0));
 
+//TODO: Make gameBoard responsive
 generateGameBoard(numberOfRows, numberOfColumns);
 
 //---------toggling cells on game Board---------------
@@ -36,152 +37,160 @@ selectCells.forEach(element => {
         } else {
             board[row][col] = 0;
         }
-        console.table(board);
-        console.log(element)
+        // console.table(board);
+        // console.log(element)
     });
 });
 
-
+//TODO: take arguments from select and pass to setSpeed function
 async function setSpeed(speed) {
     await timeout(speed);
 }
 
 
 //-------Buttons-------------
-
+//TODO: fix start stop game
 //-------start stop game button-------------
-
-
+let play = false;
 const startStopButton = document.querySelector('#start-stop-button');
+
 startStopButton.addEventListener('click', () => {
     const initialText = 'Start';
     if (startStopButton.textContent.toLowerCase().includes(initialText.toLowerCase())) {
+      for(let i=0; i < 1000; i++) {
+          setTimeout(() => { startGame(); }, 300);
+          // console.log(i)
+      }
         startStopButton.textContent = 'Stop';
     } else {
         startStopButton.textContent = initialText;
     }
+
 })
 
-// while(playgame===true){
-//     startGame();
-// }
-// async function startGame() {
-//     createLogic();
-//     updateBoard();
-//     updateGameBoard();
-//
-//     await timeout(300);
-// }
+
+function startGame() {
+    gameLogic();
+}
 
 function stopGame() {
 }
 
 //-------clear board button----------
-//TODO: fix clear button - add
+//TODO: debug clear button
 const clearButton = document.querySelector('#clear-button');
+
 clearButton.addEventListener('click', () => {
     clearBoard();
 })
 
 clearBoard = () => {
     selectCells.forEach(element => {
-        if (element.classList.contains("alive")) {
+        if (element.classList.contains('alive')) {
             element.classList.remove('alive')
         }
     });
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
             board[i][j] = 0;
+            newBoard[i][j] = 0;
         }
     }
 }
 
 //----------game logic----------------
-    function findNeighbours(row, col) {
-        let count = 0;
-        let rowNumber = Number(row);
-        let columnNumber = Number(col);
+function findNeighbours(row, col) {
+    let count = 0;
+    let rowNumber = Number(row);
+    let columnNumber = Number(col);
 
-        if (rowNumber - 1 >= 0) {
-            if (board[rowNumber - 1][columnNumber] === 1)
-                count++;
-        }
-        if (rowNumber - 1 >= 0 && columnNumber - 1 >= 0) {
-            if (board[rowNumber - 1][columnNumber - 1] === 1)
-                count++;
-        }
-        if (rowNumber - 1 >= 0 && columnNumber + 1 < numberOfColumns) {
-            if (board[rowNumber - 1][columnNumber + 1] === 1)
-                count++;
-        }
-        if (columnNumber - 1 >= 0) {
-            if (board[rowNumber][columnNumber - 1] === 1)
-                count++;
-        }
-        if (columnNumber + 1 < numberOfColumns) {
-            if (board[rowNumber][columnNumber + 1] === 1)
-                count++;
-        }
-        if (rowNumber + 1 < numberOfRows && columnNumber - 1 >= 0) {
-            if (board[rowNumber + 1][columnNumber - 1] === 1)
-                count++;
-        }
-        if (rowNumber + 1 < numberOfRows && columnNumber + 1 < numberOfColumns) {
-            if (board[rowNumber + 1][columnNumber + 1] === 1)
-                count++;
-        }
-        if (rowNumber + 1 < numberOfRows) {
-            if (board[rowNumber + 1][columnNumber] === 1)
-                count++;
-        }
-        return count;
+    if (rowNumber - 1 >= 0) {
+        if (board[rowNumber - 1][columnNumber] === 1)
+            count++;
     }
+    if (rowNumber - 1 >= 0 && columnNumber - 1 >= 0) {
+        if (board[rowNumber - 1][columnNumber - 1] === 1)
+            count++;
+    }
+    if (rowNumber - 1 >= 0 && columnNumber + 1 < numberOfColumns) {
+        if (board[rowNumber - 1][columnNumber + 1] === 1)
+            count++;
+    }
+    if (columnNumber - 1 >= 0) {
+        if (board[rowNumber][columnNumber - 1] === 1)
+            count++;
+    }
+    if (columnNumber + 1 < numberOfColumns) {
+        if (board[rowNumber][columnNumber + 1] === 1)
+            count++;
+    }
+    if (rowNumber + 1 < numberOfRows && columnNumber - 1 >= 0) {
+        if (board[rowNumber + 1][columnNumber - 1] === 1)
+            count++;
+    }
+    if (rowNumber + 1 < numberOfRows && columnNumber + 1 < numberOfColumns) {
+        if (board[rowNumber + 1][columnNumber + 1] === 1)
+            count++;
+    }
+    if (rowNumber + 1 < numberOfRows) {
+        if (board[rowNumber + 1][columnNumber] === 1)
+            count++;
+    }
+    return count;
+}
 
-
-    function gameLogic() {
-        for (let i = 0; i < board.length; i++) {
-            for (let j = 0; j < board[i].length; j++) {
-                let neighbors = findNeighbours(i, j);
-                if (board[i][j] === 1) {
-                    if (neighbors < 2) {
-                        newBoard[i][j] = 0;
-                    } else if (neighbors === 2 || neighbors === 3) {
-                        newBoard[i][j] = 1;
-                    } else if (neighbors > 3) {
-                        newBoard[i][j] = 0;
-                    }
-                } else if (board[i][j] === 0) {
-                    if (neighbors === 3) {
-                        newBoard[i][j] = 1;
-                    }
+async function gameLogic() {
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            let neighbors = findNeighbours(i, j);
+            if (board[i][j] === 1) {
+                if (neighbors < 2) {
+                    newBoard[i][j] = 0;
+                } else if (neighbors === 2 || neighbors === 3) {
+                    newBoard[i][j] = 1;
+                } else if (neighbors > 3) {
+                    newBoard[i][j] = 0;
                 }
-                //add here updateBoard?
-            }
-        }
-    }
-
-    function updateBoard() {
-        for (let i = 0; i < board.length; i++) {
-            for (let j = 0; j < board[i].length; j++) {
-                board[i][j] = newBoard[i][j];
-                newBoard[i][j] = 0;
-            }
-        }
-    }
-
-    function updateGameBoard() {
-        let cell = '';
-        for (let i = 0; i < board.length; i++) {
-            for (let j = 0; j < board[i].length; j++) {
-                cell = document.getElementById(j + '-' + i);
-                if (board[i][j] === 0) {
-                    cell.setAttribute('class', 'row dead');
-                } else {
-                    cell.setAttribute('class', 'row alive');
+            } else if (board[i][j] === 0) {
+                if (neighbors === 3) {
+                    newBoard[i][j] = 1;
                 }
             }
+            //add here updateBoard?
         }
     }
 
 
-    const timeout = async time => await new Promise(resolve => setTimeout(() => resolve(), time));
+    updateBoard();
+    updateGameBoard();
+    await timeout(300);
+}
+
+async function updateBoard() {
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            board[i][j] = newBoard[i][j];
+            newBoard[i][j] = 0;
+        }
+    }
+    await timeout(300);
+}
+
+async function updateGameBoard() {
+    let cell = '';
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            cell = document.getElementById(j + '-' + i);
+            if (board[i][j] === 0) {
+                cell.setAttribute('class', 'row dead');
+            } else {
+                cell.setAttribute('class', 'row alive');
+            }
+        }
+    }
+    await timeout(300);
+
+}
+
+
+const timeout = async time => await new Promise(resolve => setTimeout(() => resolve(), time));
